19512ce37f572b87a282a58dd027411308c3a991
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-09-11 11:08:26 +0300

    Добавьте файлы проекта.

---
+++ Homework1_3_4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CaseExit = "exit";
+            const string CaseSum = "sum";
+            int arrayLength = 0;
+            int[] array = new int[arrayLength];
+            bool isEndProgram = false;
+            int arraySum;
+
+            while (isEndProgram == false)
+            {
+                Console.Clear();
+                Console.WriteLine("Вы можете ввести либо число, которое добавится в массив, либо команду sum, которая высчитает сумму чисел в массиве, либо команду exit, для выхода из программы.");
+                Console.WriteLine();
+                Console.WriteLine("Введите команду или число: ");
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case CaseSum:
+                        arraySum = 0;
+
+                        for(int arrayIndex=0; arrayIndex < array.Length; arrayIndex++)
+                        {
+                            arraySum+=array[arrayIndex];
+                        }
+
+                        Console.WriteLine("Сумма элементов массива: " + arraySum);
+                        Console.ReadKey();
+                        break;
+                    case CaseExit:
+                        isEndProgram = true;
+                        break;
+                    default:
+                        int[] arrayTemp = new int[array.Length + 1];
+
+                        for (int arrayIndex=0; arrayIndex<array.Length; arrayIndex++)
+                        {
+                            arrayTemp[arrayIndex] = array[arrayIndex];
+                        }
+
+                        arrayTemp[arrayTemp.Length-1]= Convert.ToInt32(command);
+                        array = arrayTemp;
+                        break;
+                }
+            }
+        }
+    }
+}
